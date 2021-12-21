ce5c5d1ec60e443c3cf118bc9e8be0d210fe1ffa
natade-jp <natade3@gmail.com>
2021-12-21 22:08:59 +0900

    ひな形

---
+++ src/Chikuchiku2Fuwafuwa4TeamsMod.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Chikuchiku2Fuwafuwa4TeamsMod
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Aim for a mod that converts tingling words into fluffy words within Teams.
+// @author       natade-jp
+// @match        https://teams.microsoft.com/*/conversations/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
